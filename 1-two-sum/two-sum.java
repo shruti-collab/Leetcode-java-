fab00a97561c1b40ef8e2a13346d344a1c9a5166
Shruti Kumari <shrutikumari916200@gmail.com>
2026-06-03 23:47:19 +0530

    Time: 2 ms (99.39%) | Memory: 47.4 MB (11.00%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,36 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-        // for (int i=0;i<nums.length;i++){
-        //     for(int j=i+1;j<nums.length;j++){
-        //         if (nums[i] == target-nums[j]){
-        //             return new int[]{i,j};
-        //         }
-        //     }
-           
-        // }
-        //  return null;
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
 
-        // // hashmap approach
-        // Map<Integer,Integer>map= new HashMap<>();
-        // for(int i=0;i<nums.length;i++){
-        //     map.put(nums[i],i);
-        // }
-        // for(int i=0;i<nums.length;i++){
-        //     int remain= target - nums[i];
-        //     if(map.containsKey(remain)&& map.get(remain)!=i){
-        //         return new int[]{i,map.get(remain)};
-        //     }
-        // }
-        // return null;
-        Map<Integer, Integer>map= new HashMap();
-        for(int i=0;i<nums.length;i++){
-            int remain= target-nums[i];
-            if(map.containsKey(remain)){
-                return new int[]{i,map.get(remain)};
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
             }
-            map.put(nums[i],i);
+            numMap.put(nums[i], i);
         }
-        return null;
+
+        return new int[]{}; // No solution found
     }
 }
