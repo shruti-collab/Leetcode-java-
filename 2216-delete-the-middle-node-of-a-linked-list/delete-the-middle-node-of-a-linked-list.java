24941ae62504d1fbc605c0f5383fff40334ac6c3
Shruti Kumari <shrutikumari916200@gmail.com>
2026-06-16 00:03:06 +0530

    Time: 3 ms (99.96%) | Memory: 202.7 MB (22.05%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
@@ -10,7 +10,7 @@
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        if(head.next==null){
+        if(head.next==null|| head==null){
             return null;
         }
 
