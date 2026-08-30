453de92ab8aef1ece4a18febc2dce5fd41fd37c1
Shruti Kumari <shrutikumari916200@gmail.com>
2026-08-31 00:32:23 +0530

    Time: 2 ms (99.81%) | Memory: 46.4 MB (59.72%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,38 +1,32 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(s.equals(t)) return s;
-        if(s.length() < t.length()) return "";
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(char a : t.toCharArray()){
-            map.merge(a,1,Integer::sum);
+        if (s == null || t == null || s.length() ==0 || t.length() == 0 ||
+                s.length() < t.length()) {
+            return new String();
         }
-        int i=0,j=0;
-        int ans = Integer.MAX_VALUE;
-        int count  = map.size();
-        int start = i;
-        while(j<s.length()){
-            char c = s.charAt(j);
-            if(map.containsKey(c)){
-                map.put(c,map.get(c)-1);
-                if(map.get(c)==0){
-                    count--;
+        int[] map = new int[128];
+        int count = t.length();
+        int start = 0, end = 0, minLen = Integer.MAX_VALUE,startIndex =0;
+        for (char c :t.toCharArray()) {
+            map[c]++;
+        }
+        char[] chS = s.toCharArray();
+        while (end < chS.length) {
+            if (map[chS[end++]]-- >0) {
+                count--;
+            }
+            while (count == 0) {
+                if (end - start < minLen) {
+                    startIndex = start;
+                    minLen = end - start;
+                }
+                if (map[chS[start++]]++ == 0) {
+                    count++;
                 }
             }
-            while(count == 0){
-                if(j-i+1 < ans){
-                    ans = j-i+1;
-                    start = i;
-                }
-                
-                char left = s.charAt(i);
-                if(map.containsKey(left)){
-                    map.put(left,map.get(left)+1);
-                    if(map.get(left)>0) count++;
-                }
-                i++;
-            }
-            j++;
         }
-        return ans == Integer.MAX_VALUE ? "" : s.substring(start,start+ans);
+
+        return minLen == Integer.MAX_VALUE? new String():
+                new String(chS,startIndex,minLen);
     }
 }
