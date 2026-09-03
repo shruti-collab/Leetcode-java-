8e524743f9faf8b5aae370bbe6b89f8738bc37ca
Shruti Kumari <shrutikumari916200@gmail.com>
2026-09-04 00:43:45 +0530

    Time: 3 ms (88.13%) | Memory: 52.1 MB (67.34%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,72 +1,29 @@
-class Solution {
-    static {
-        Runtime.getRuntime().gc();
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (FileWriter writer = new FileWriter("display_runtime.txt")) {
-                writer.write("0");
-                    } 
-            catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }));
-        }
+public class Solution {
+    private int n;
+    private int m;
 
     public int numIslands(char[][] grid) {
-         if( grid==null|| grid.length==0){
-            return 0;
-
-         }
-          
-          int row= grid.length;
-          int column= grid[0].length;
-          int count=0;
-
-           for(int r=0; r< row; r++){
-             for( int c=0; c< column; c++){
-                if( grid[r][c]=='1'){
-                    bfs(grid, r,c);
-                    count++;
-                }
-             }
-           }    
-            return count;  
-             
-         }
-    
-     void bfs(char[][] grid, int sr, int sc) {
-
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{sr, sc});
-
-        grid[sr][sc] = '0'; 
-
-        int[][] directions = {{1,0},{-1, 0},{0,1},{0,-1} };
-
-        while (!queue.isEmpty()) {
-
-            int[] cell = queue.poll();
-            int r = cell[0];
-            int c = cell[1];
-
-            for (int[] dir : directions) {
-
-                int newR = r + dir[0];
-                int newC = c + dir[1];
-
-                if (newR >= 0 && newR < grid.length &&
-                    newC >= 0 && newC < grid[0].length &&
-                    grid[newR][newC] == '1') {
-
-                    grid[newR][newC] = '0';
-                    queue.add(new int[]{newR, newC});
+        int count = 0;
+        n = grid.length;
+        if (n == 0) return 0;
+        m = grid[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1') {
+                    DFSMarking(grid, i, j);
+                    ++count;
                 }
             }
         }
-        
+        return count;
+    }
+
+    private void DFSMarking(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1') return;
+        grid[i][j] = '0';
+        DFSMarking(grid, i + 1, j);
+        DFSMarking(grid, i - 1, j);
+        DFSMarking(grid, i, j + 1);
+        DFSMarking(grid, i, j - 1);
     }
 }
-
-
-  
-
-
