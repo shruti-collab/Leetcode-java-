2c7f17fd8d5eaac3a218b9499d015a8baabe5e58
Shruti Kumari <shrutikumari916200@gmail.com>
2026-09-24 00:14:57 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (94.65%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,26 +1,23 @@
 class Solution {
     public int trap(int[] height) {
-        int left=0, right= height.length-1;
-        int leftMax=0, rightMax=0;
-        int totalWater = 0;
-        while(left < right){
-            if(height[left] <= height[right]){
-                if(leftMax < height[left]){
-                    leftMax= height[left];
-                }else{
-                    totalWater += leftMax - height[left];
-                }
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = height[left];
+        int rightMax = height[right];
+        int water = 0;
+
+        while (left < right) {
+            if (leftMax < rightMax) {
                 left++;
-            }else{
-                if(rightMax < height[right]){
-                    rightMax=height[right];
-                }else{
-                    totalWater += rightMax - height[right];
-                }
+                leftMax = Math.max(leftMax, height[left]);
+                water += leftMax - height[left];
+            } else {
                 right--;
+                rightMax = Math.max(rightMax, height[right]);
+                water += rightMax - height[right];
             }
         }
 
-        return totalWater;
+        return water;        
     }
 }
